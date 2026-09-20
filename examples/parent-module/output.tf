d3740f3b850031c3d149bab1687ed1a8b51f43a8
Hari26Here <hari26subramaninam@gmail.com>
2026-09-20 21:00:26 +0530

    terraform validate output fix for parent module

--- examples/parent-module/output.tf
+++ examples/parent-module/output.tf
@@ -3,12 +3,7 @@ output "rg_name" {
   description = "The name of the resource group"
 }
 
-output "rg_location" {
-  value = module.rg_module1.location
-  description = "The location where the resource group is created"
-}
-
 output "rg_id" {
-  value = module.rg_module1.id
+  value = module.rg_module1.rg_id
   description = "ID of the azure resource group"
 }
